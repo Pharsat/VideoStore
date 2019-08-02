21803b8272ec5a12fbb576c63fe745ba6be9c4f2
Cristian Felipe Gallego Ramirez <cristian.gallego@globant.com>
2019-08-02 18:29:55 -0500

    extracting thead to another compnent.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -50,14 +50,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSort = path => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path) {
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    } else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
+  handleSort = sortColumn => {
     this.setState({ sortColumn });
   };
 
@@ -104,6 +97,7 @@ class Movies extends Component {
             <h1>Showing {count} movies in the database</h1>
             <MoviesList
               movies={paginatedMovies}
+              sortColumn={sortColumn}
               onDelete={this.handleDelete}
               onLike={this.handleLike}
               onSort={this.handleSort}
--- src/components/moviesList.jsx
+++ src/components/moviesList.jsx
@@ -1,32 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import MovieDetail from "./movieDetail";
-const MoviesList = props => {
-  const { movies, onDelete, onLike, onSort } = props;
+import TableHeader from "./common/tableHeader";
 
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th onClick={() => onSort("title")}>Tittle</th>
-          <th onClick={() => onSort("genre.name")}>Genre</th>
-          <th onClick={() => onSort("numberInStock")}>Stock</th>
-          <th onClick={() => onSort("dailyRentalRate")}>Rate</th>
-          <th>Like</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {movies.map(movie => (
-          <MovieDetail
-            key={movie._id}
-            movie={movie}
-            onDelete={onDelete}
-            onLike={onLike}
-          />
-        ))}
-      </tbody>
-    </table>
-  );
-};
+class MoviesList extends Component {
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    { key: "like" },
+    { key: "empty" }
+  ];
+
+  render() {
+    const { sortColumn, movies, onDelete, onLike, onSort } = this.props;
+
+    return (
+      <table className="table">
+        <TableHeader
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
+
+        <tbody>
+          {movies.map(movie => (
+            <MovieDetail
+              key={movie._id}
+              movie={movie}
+              onDelete={onDelete}
+              onLike={onLike}
+            />
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default MoviesList;
